d460cfe98f5e2c28b3ce6295421bb5f5a1ffb346
Jakob Bossek <info@jakobbossek.de>
2015-01-21 15:26:28 +0100

    fix bug in morphInstances, add visualizeMorphing

--- R/morphInstances.R
+++ R/morphInstances.R
@@ -44,14 +44,17 @@ morphInstances = function(x, y, alpha) {
         stopf("Both or none of the instances must have depots")
     }
 
+    types = rep("customers", getNumberOfNodes(x))
+
     if (all(hasDepots(x), hasDepots(y))) {
         depot.idx = which(x$types == "depot")
         x.coordinates = x.coordinates[-depot.idx, , drop = FALSE]
         y.coordinates = y.coordinates[-depot.idx, , drop = FALSE]
+        n.depots = getNumberOfDepots(x)
+        types = c(rep("depot", n.depots), rep("customers", getNumberOfNodes(x) - n.depots))
     }
 
     coordinates = getPointMatchingAndMorphCoordinates(x.coordinates, y.coordinates)
-    types = rep("customers", getNumberOfNodes(x) - 2)
 
     if (all(hasDepots(x), hasDepots(y))) {
         x.n.depots = getNumberOfDepots(x)
@@ -64,13 +67,76 @@ morphInstances = function(x, y, alpha) {
         y.depot.coordinates = getDepotCoordinates(y)
         depot.coordinates = getPointMatchingAndMorphCoordinates(x.depot.coordinates, y.depot.coordinates)
         coordinates = rbind(depot.coordinates, coordinates)
-        types = c(rep("depot", x.n.depots), types)
     }
     z = makeNetwork(coordinates = coordinates, types = types, lower = x$lower, upper = x$upper)
     # FIXME: ugly to do that here
     attr(z, "morphed") = TRUE
     attr(z, "morphing.grade") = alpha
     return(z)
+}
+
+#' Fancy visualization of morphing.
+#'
+#' Takes two instances of equal size and some alphas. Computes the point matching
+#' and morphings and visualizes the points of the first instance gradually move
+#' towards their matched counterparts of the second instance.
+#'
+#' @param x [\code{Network}]\cr
+#'   Network or ClusteredNetwork.
+#' @param y [\code{Network}]\cr
+#'   Network or ClusteredNetwork.
+#' @param alphas [\code{numeric}]\cr
+#'   Coeffiecients 'alpha' for different convex combinations.
+#' @param arrows [\code{logical(1)}]\cr
+#'   Draw arrows originating in the points of \code{x} and ending in the
+#'   points matched in \code{y}. Default is \code{TRUE}.
+#' @return [\code{\link[ggplot2]{ggplot}}]
+#' @seealso \code{\link{morphInstances}}
+#' @export
+visualizeMorphing = function(x, y, alphas = c(0.25, 0.5, 0.75), arrows = TRUE) {
+    assertClass(x, "Network")
+    assertClass(y, "Network")
+    assertNumeric(alphas, any.missing = FALSE, lower = 0, upper = 0.75)
+
+    # we compute the point matching here one time additionaly for later use
+    point.matching = getOptimalPointAssignment(x$coordinates, y$coordinates)
+
+    # do the morphing
+    instances = lapply(alphas, function(alpha) {
+        morphInstances(x, y, alpha = alpha)
+    })
+
+    # put in the original instances as extreme instances
+    alphas = c(0, alphas, 1)
+    instances = c(list(x), instances, list(y))
+
+    # build data frame of points
+    df.points = lapply(1:length(instances), function(i) {
+        df = as.data.frame(instances[[i]], include.extra = FALSE)
+        df$alpha = alphas[i]
+        df
+    })
+
+    df.points = do.call(rbind, df.points)
+    df.points$alpha = as.factor(df.points$alpha)
+
+    pl = ggplot(data = df.points, mapping = aes_string(x = "x1", y = "x2", colour = "alpha"))
+
+    if (arrows) {
+        # build arrows from x to (matched) y coordinates
+        df.arrows = cbind(as.data.frame(x$coordinates), as.data.frame(y$coordinates[point.matching[, 2], ]))
+        colnames(df.arrows) = c("x1", "x2", "end1", "end2")
+        pl = pl + geom_segment(
+            data = df.arrows,
+            mapping = aes_string(x = "x1", y = "x2", xend = "end1", yend = "end2"),
+                arrow = grid::arrow(type = "closed", length = grid::unit(0.1, "inches")),
+                colour = "gray"
+            )
+    }
+
+    pl = pl + geom_point()
+    #pl = pl + geom_point(data = subset(df.points, alpha %in% c(0, 1)), colour = "black", size = 3)
+    return(pl)
 }
 
 # Visualize point matching.
