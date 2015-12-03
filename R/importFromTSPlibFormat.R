e021f71c80518c5a3d77309a0684cdbe448c48af
MartinWolke <martin.woelck@gmail.com>
2015-12-03 13:02:29 +0100

    added edge.type to Network. Altered import and export correspondingly.

--- R/importFromTSPlibFormat.R
+++ R/importFromTSPlibFormat.R
@@ -54,7 +54,8 @@ importFromTSPlibFormat = function(filename, round.distances = TRUE) {
     distance.matrix = network$edge_weights,
     lower = if (!is.null(network$lower)) as.numeric(network$lower) else NULL,
     upper = if (!is.null(network$upper)) as.numeric(network$upper) else NULL,
-    membership = network$membership
+    membership = network$membership,
+    edge.weight = network$edge_weight_type
   )
 }
 
