ec2c381a65b40d1d95659af3a975f716b1b5711d
Jakob Bossek <info@jakobbossek.de>
2016-01-09 21:43:14 +0100

    rescaleNetwork now recomputes distance matrix

--- R/rescaleNetwork.R
+++ R/rescaleNetwork.R
@@ -1,6 +1,7 @@
 #' @title Rescale network
 #'
-#' @description Normalize network coordinates to the unit cube maintaining its geography.
+#' @description Normalize network coordinates to the unit cube while maintaining
+#' its geography.
 #'
 #' @template arg_network
 #' @param method [\code{character(1)}]\cr
@@ -19,14 +20,15 @@
 #' library(gridExtra)
 #' x = generateClusteredNetwork(n.points = 100L, n.cluster = 4L, name = "Rescaling Demo")
 #'
-#' # here we "stretch" the instance x direction
+#' # here we "stretch" the instance x direction to visualize the differences of
+#' # the rescaling mehtods
 #' x$coordinates[, 1] = x$coordinates[, 1] * 10L
 #' x$upper = x$upper * 10L
 #' pls = list(
-#'     autoplot(x) + ggtitle("Original"),
-#'     autoplot(rescaleNetwork(x, method = "by.dimension")) + ggtitle("By dimension"),
-#'     autoplot(rescaleNetwork(x, method = "global")) + ggtitle("Global"),
-#'     autoplot(rescaleNetwork(x, method = "global2")) + ggtitle("Global2")
+#'   autoplot(x) + ggtitle("Original"),
+#'   autoplot(rescaleNetwork(x, method = "by.dimension")) + ggtitle("By dimension"),
+#'   autoplot(rescaleNetwork(x, method = "global")) + ggtitle("Global"),
+#'   autoplot(rescaleNetwork(x, method = "global2")) + ggtitle("Global2")
 #' )
 #' pls$nrow = 1L
 #' do.call(grid.arrange, pls)
@@ -60,6 +62,10 @@ rescaleNetwork = function(x, method = "global2") {
     x$coordinates = rescaled.coordinates
   }
 
+  # distance matrix needs an update (probably there is a nice formula for that
+  # without O(n^2) recomputation of the distance matrix)
+  #x$distance.matrix = as.matrix(dist(x))
+
   # rescaling is a normalization to [0,1]^dim
   x$lower = 0
   x$upper = 1
@@ -69,7 +75,7 @@ rescaleNetwork = function(x, method = "global2") {
 # Rescale by normalizing with global min/max values.
 rescaleNetworkGlobal = function(x) {
   rg = range(x)
-  (x - rg[1]) / (rg[2] - rg[1])
+  return((x - rg[1]) / (rg[2] - rg[1]))
 }
 
 # Rescale by shifting dimension-wise and scaling with the maximal range
@@ -80,12 +86,12 @@ rescaleNetworkGlobal2 = function(x) {
   x[, 1] = (x[, 1] - rgs[1, 1])
   x[, 2] = (x[, 2] - rgs[1, 2])
   scale = max(rgs[2,] - rgs[1,])
-  x / scale
+  return(x / scale)
 }
 
 # Rescale by normalizing dimension-wise with min/max values
 rescaleNetworkByDimension = function(x) {
   min = apply(x, 2, min)
   max = apply(x, 2, max)
-  t((t(x) - min) / (max - min))
+  return(t((t(x) - min) / (max - min)))
 }
